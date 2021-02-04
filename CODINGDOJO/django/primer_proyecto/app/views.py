d90f17a98e7e836fceeef2b00b45a5e3b9d2ad49
camilariquelme <72618195+camilariquelme@users.noreply.github.com>
2021-02-04 20:12:17 -0300

    040221

---
+++ CODINGDOJO/django/primer_proyecto/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse, redirect
+def index(request):
+    return HttpResponse("placeholder para luego mostrar una lista de todos los blogs")
+def root(request):
+    return redirect("/blogs")
+def new(request):
+    return HttpResponse("placeholder para mostrar un nuevo formulario para crear un nuevo blog")
+def create(request):
+    return redirect("/")
+def show(request, number):
+    return HttpResponse(f"placeholder para mostrar el blog numero: {number}")
+def edit(request, number):
+    return HttpResponse(f"placeholder para editar el blog {number}")
+def destroy(request, number):
+    return redirect("/blogs")
